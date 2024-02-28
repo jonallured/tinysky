bb90bdc5cf4f7b810c76ec2a648b57b9144b1c99
Jon Allured <jon@jonallured.com>
2024-02-28 08:04:10 -0600

    Initial commit

---
+++ lib/tinysky.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "tinysky/version"
+
+module Tinysky
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/tinysky_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Tinysky do
+  it "has a version number" do
+    expect(Tinysky::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
