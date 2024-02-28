81123b1a1a4c6e2c767f6ce66ee9202a2eb6da5f
Jon Allured <jon@jonallured.com>
2024-02-28 08:28:38 -0600

    Remove placeholder test

--- spec/tinysky_spec.rb
+++ spec/tinysky_spec.rb
@@ -1,9 +1,5 @@
-RSpec.describe Tinysky do
+describe Tinysky do
   it "has a version number" do
     expect(Tinysky::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
   end
 end
