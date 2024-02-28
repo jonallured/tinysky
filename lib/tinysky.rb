476ea5ba18f17a7c7819976001af5459cca83889
Jon Allured <jon@jonallured.com>
2024-02-28 08:16:16 -0600

    Remove frozen string comments

--- lib/tinysky.rb
+++ lib/tinysky.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 require_relative "tinysky/version"
 
 module Tinysky
--- spec/tinysky_spec.rb
+++ spec/tinysky_spec.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 RSpec.describe Tinysky do
   it "has a version number" do
     expect(Tinysky::VERSION).not_to be nil
