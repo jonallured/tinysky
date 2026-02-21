4f92a9071551a9a0f2002acace7721b38e394b2a
Jon Allured <jon@jonallured.com>
2026-02-21 09:31:53 -0600

    Add client method to upload blob

--- lib/tinysky/endpoints.rb
+++ lib/tinysky/endpoints.rb
@@ -1,6 +1,7 @@
 module Tinysky
   module Endpoints
     REPO_CREATE_RECORD = "/xrpc/com.atproto.repo.createRecord"
+    REPO_UPLOAD_BLOB = "/xrpc/com.atproto.repo.uploadBlob"
     SERVER_CREATE_SESSION = "/xrpc/com.atproto.server.createSession"
   end
 end
